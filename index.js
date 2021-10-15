f3b00c9b2ee8bf9f2786f04069e245f95fe7b42b
Santi Cano <canosantiago404@gmail.com>
2021-10-15 20:12:07 -0300

    added gameloop

--- index.js
+++ index.js
@@ -21,3 +21,174 @@ let cpuShips = [
 
 let isGameOver = false;
 let isPlayerTurn = true;
+
+const cpuAttackHitLocation = { x: null, y: null, tried: 0 };
+
+function checkCoordinateValidity(gameboard, item, x, y, isRotated) {
+    const shipLength = item.getShipLength();
+    if (gameboard.getBoardArray()[x][y] !== "ocean") {
+        return false;
+    }
+
+    if (isRotated) {
+        if (x + shipLength - 1 > 9) {
+            return false;
+        } else {
+            for (let i = -1; i <= shipLength; i++) {
+                if (
+                    gameboard.getBoardArray()[x + i < 0 || x + i > 9 ? x : x + i][
+                        y + 1 > 9 ? y : y + 1
+                    ] !== "ocean" ||
+                    gameboard.getBoardArray()[x + i < 0 || x + i > 0 ? x : x + i][y] !==
+                        "ocean" ||
+                    gameboard.getBoardArray()[x + i < 0 || x + i > 9 ? x : x + i][
+                        y - 1 < 0 ? y : y - 1
+                    ] !== "ocean"
+                ) {
+                    return false;
+                }
+            }
+        }
+    } else {
+        if  (y + shipLength - 1 > 9) {
+            return false;
+        } else {
+            for (let i = -1; i <= shipLength; i++) {
+                if (
+                  gameboard.getBoardArray()[x + 1 > 9 ? x : x + 1][
+                    y + i < 0 || y + i > 9 ? y : y + i
+                  ] !== "ocean" ||
+                  gameboard.getBoardArray()[x][y + i < 0 || y + i > 9 ? y : y + i] !==
+                    "ocean" ||
+                  gameboard.getBoardArray()[x - 1 < 0 ? x : x - 1][
+                    y + i < 0 || y + i > 9 ? y : y + i
+                  ] !== "ocean"
+                ) {
+                  return false;
+            }
+          }
+        }
+    }
+
+    return true;
+}
+
+function positioningOfShips(gb, ships) {
+    function randomizeStuff() {
+        let xRandom = Math.floor(Math.random() * 10);
+        let yRandom = Math.floor(Math.random() * 10);
+        let rotationRandom = !!Math.floor(Math.random() * 2);
+        return { xRandom, yRandom, rotationRandom };
+    }
+
+    ships.forEach((item) => {
+        let isValid = false;
+        let {
+            xRandom: x,
+            yRandom: y,
+            rotationRandom: isRotated,
+        } = randomizeStuff();
+        while (!isValid) {
+            if (checkCoordinateValidity(gb, item, x, y, isRotated)) {
+                isValid = true;
+            } else {
+                ({
+                    xRandom: x,
+                    yRandom: y,
+                    rotationRandom: isRotated,
+                } = randomizeStuff()); 
+            }
+
+            item.getShipLength();
+        }
+        gb.setShipCoordinates(item, x, y, isRotated);
+    });
+}
+
+function gameLoop() {
+    const main = document.querySelector("main");
+    main.classList.remove("main-element");
+    main.classList.add("game-main");
+    const playerTablePlaceholder = document.createElement("div");
+    const cpuTablePlaceholder = document.createElement("div");
+
+    playerTablePlaceholder.dataset.name = "La computadora esta atacando...";
+    cpuTablePlaceholder.dataset.name = "El jugador esta atacando...";
+
+    if (!document.querySelector(".game-status")) {
+        const gameStatus = document.createElement("div");
+        gameStatus.className = "game-status";
+        gameStatus.textContent = "Tu turno";
+        main.append(gameStatus);
+    } else {
+        const gameStatus = document.querySelector(".game-status");
+        gameStatus.textContent = `${isPlayerTurn ? "Tu" : "CPU"} turno.`;
+    }
+
+    playerTablePlaceholder.classList.add(
+        "table-placeholder",
+        "placeholder-player"
+    );
+    cpuTablePlaceholder.classList.add("table-placeholder", "placeholder-cpu");
+
+    if (isPlayerTurn) {
+        cpuTablePlaceholder.classList.add("turn");
+    } else playerTablePlaceholder.classList.add("turn");
+
+    main.append(playerTablePlaceholder, cpuTablePlaceholder);
+
+    const playerTable = document.createElement("table");
+    const cpuTable = document.createElement("table");
+
+    playerTable.classList.add("table-player");
+    cpuTable.classList.add("table-cpu");
+
+    function generateTable(placeholder, gb, table) {
+        let i = 0;
+        gb.getBoardArray().forEach((row) => {
+            let j = 0;
+            const tableRow = document.createElement("tr");
+            table.append(tableRow);
+            row.forEach((ele) => {
+                const data = document.createElement("td");
+
+                if (table.classList.contains("table-player")) {
+                    if (
+                        gb.getBoardArray()[i][j] &&
+                        typeof gb.getBoardArray()[i][j] === "object"
+                    ) {
+                        data.classList.add("occupied");
+                    }
+                }
+
+                if (gb.getBoardArray()[i][j] === "miss") {
+                    data.classList.add("miss");
+                } else if (gb.getBoardArray()[i][j].isSunk) {
+                    data.classList.add("sunk");
+                } else if (gb.getBoardArray()[i][j].isHit) {
+                    data.classList.add("hit");
+                }
+
+                if (isPlayerTurn && table.classList.contains("table-cpu")) {
+                    data.addEventListener("click", (e) => {
+                        tileClickEvent(e, gb);
+                    });
+                }
+                data.dataset.x = i;
+                data.dataset.y = j;
+                tableRow.append(data);
+                j++;
+            });
+            i++;
+        });
+        placeholder.append(table);
+    }
+
+    generateTable(playerTablePlaceholder, playerGB, playerTable);
+    generateTable(cpuTablePlaceholder, cpuGB, cpuTable);
+
+    if (!isPlayerTurn) {
+        cpuAttackHitLocation(playerGB);
+    }
+}
+
